568f22366cb14c21d7d20a0d065bdf61835e4f77
RaajSahabG <vishalrajsatsangi2003@gmail.com>
2025-12-30 11:30:50 +0530

    FEAT: change the route of save contact

--- routes/userContactRoute.js
+++ routes/userContactRoute.js
@@ -5,6 +5,6 @@ import role from '../middlewares/roles.js';
 const router = express.Router();
 
 router.post("/", protect, role('customer'), syncContacts);
-router.post("/save-chat", protect, saveChatContact);
+router.post("/save-contact", protect, saveChatContact);
 
 export default router;
